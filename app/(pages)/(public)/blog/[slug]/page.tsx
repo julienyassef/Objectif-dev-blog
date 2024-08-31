0b85c18f813b5444f7114fe793a920060f93463a
julien yassef <juyassef@gmail.com>
2024-08-31 21:35:43 +0200

    amélioeration SEO page article blog

--- app/(pages)/(public)/blog/[slug]/page.tsx
+++ app/(pages)/(public)/blog/[slug]/page.tsx
@@ -9,6 +9,7 @@ import useArticles from '@/hooks/useArticles';
 import RelatedArticleCard from '@/components/Cards/RelatedArticleCard/RelatedArticleCard';
 import ShareButton from '@/components/Buttons/ShareButton/ShareButton';
 import LikeButton from '@/components/Buttons/LikeButton/LikeButton';
+import Head from 'next/head';
 
 interface ArticlePageProps {
   params: {
@@ -47,9 +48,7 @@ const ArticlePage = ({ params }: ArticlePageProps) => {
 
     // Trier les autres articles en fonction du nombre de tags partagés
     const sortedArticles = articles
-    // Exclure l'article actuellement affiché de la liste des articles à comparer.
       .filter(a => a.slug !== params.slug)
-      // Pour chaque article, calculer le nombre de tags qu'il partage avec l'article actuellement affiché
       .map(a => ({
         ...a,
         sharedTagsCount: (a.tags || []).filter(tag => articleTags.has(tag)).length,
@@ -71,16 +70,25 @@ const ArticlePage = ({ params }: ArticlePageProps) => {
     const photoElement = article.content.find((element) => element.type === 'photo');
 
     return (
-      <div className="container mx-auto  mt-[166.88px] md:mt-[122.88px] lg:mt-[78.88px]">
-        <div className="flex flex-col">
-          <div className="w-full ">
-            <div className="relative w-full h-auto">
+      <>
+        <Head>
+          <title>{article.title} - Objectif Dev Blog</title>
+          <meta name="description" content={article.title || "Découvrez cet article passionnant sur Objectif Dev Blog"} />
+          <meta property="og:title" content={article.title} />
+          <meta property="og:description" content={article.title|| "Découvrez cet article passionnant sur Objectif Dev Blog"} />
+          <meta property="og:image" content={photoElement ? photoElement.value : "https://objectifdev.fr/assets/default-image.png"} />
+          <meta property="og:url" content={`https://objectifdev.fr/blog/${article.slug}`} />
+          <link rel="canonical" href={`https://objectifdev.fr/blog/${article.slug}`} />
+        </Head>
+        <div className="container mx-auto mt-[166.88px] md:mt-[122.88px] lg:mt-[78.88px]">
+          <div className="flex flex-col">
+            <div className="w-full">
               {photoElement && (
                 <div className="relative w-full h-auto">
                   <div className="relative w-full h-0" style={{ paddingBottom: '50%' }}> 
                     <Image 
                       src={photoElement.value} 
-                      alt="" 
+                      alt={article.title} 
                       layout="fill" 
                       objectFit="cover" 
                       className="w-full h-auto"
@@ -88,7 +96,9 @@ const ArticlePage = ({ params }: ArticlePageProps) => {
                   </div>
                   <div className="absolute inset-0 flex items-center justify-center">
                     <div className="bg-white bg-opacity-75 p-4 w-3/4 max-w-md text-center">
-                      <h1 className="text-2xl  md:text-4xl  font-bold text-gray-900 leading-relaxed md:leading-relaxed">{article.title}</h1>
+                      <h1 className="text-2xl md:text-4xl font-bold text-gray-900 leading-relaxed md:leading-relaxed">
+                        {article.title}
+                      </h1>
                     </div>
                   </div>
                 </div>
@@ -101,21 +111,20 @@ const ArticlePage = ({ params }: ArticlePageProps) => {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col lg:flex-row">
-            <div className="lg:w-2/3 w-full">
-              <ArticlePageComponent article={article} />
-            </div>
-            <div className="lg:w-1/3 w-full pr-6 mt-8 lg:mt-0 lg:ml-8">
-              <h3 className="text-2xl text-center font-semibold mt-4 mb-4">Parcourez d&#39;autres articles</h3>
-              {relatedArticles.map((relatedArticle) => (
-                <RelatedArticleCard key={relatedArticle.slug} article={relatedArticle} />
-              ))}
+            <div className="flex flex-col lg:flex-row">
+              <article className="lg:w-2/3 w-full">
+                <ArticlePageComponent article={article} />
+              </article>
+              <aside className="lg:w-1/3 w-full pr-6 mt-8 lg:mt-0 lg:ml-8">
+                <h3 className="text-2xl text-center font-semibold mt-4 mb-4">Parcourez d&apos;autres articles</h3>
+                {relatedArticles.map((relatedArticle) => (
+                  <RelatedArticleCard key={relatedArticle.slug} article={relatedArticle} />
+                ))}
+              </aside>
             </div>
           </div>
         </div>
-
-      </div>
+      </>
     );
   }
 };
